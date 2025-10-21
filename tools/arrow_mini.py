2d0c9bf827f2087730b3a0bcecd3ae34d6d6418e
JokerQianwei <1174665346@qq.com>
2025-10-21 20:56:08 +0800

    updata arrow_mini.py

--- tools/arrow_mini.py
+++ tools/arrow_mini.py
@@ -1,26 +1,23 @@
-import pandas as pd
 import pyarrow as pa
 import pyarrow.ipc as ipc
-import os
-import sys
+from smiles_bd.tokenizer import RegexSmilesTokenizer
+import numpy as np
+tok = RegexSmilesTokenizer("vocab.txt")
 
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# 使用绝对路径
+file_path = '/data/yqw/bd3lms-alpha/data/DrugLikeSMILSE_packed1024_butina_s055_blocks10k/train/data-01615-of-01616.arrow'
 
-# 1. 定义文件路径
-file_path = 'data/DrugLikeSMILES_packed1024_debug/train/data-00000-of-01616.arrow'
-
-# 2. 以兼容模式读取文件
 with open(file_path, "rb") as f:
     try:
-        # 优先尝试作为 File 格式读取 (用于磁盘存储)
         reader = ipc.open_file(f)
     except pa.ArrowInvalid:
-        # 如果失败，则回退到 Stream 格式 (用于流式传输)
-        f.seek(0) # 必须将文件指针移回开头
+        f.seek(0)
         reader = ipc.open_stream(f)
     
-    # 3. 从 reader 中一次性读取所有数据为 Arrow Table，然后转为 Pandas DataFrame
     df = reader.read_all().to_pandas()
 
-# 4. 打印 DataFrame 的头部信息
 print(df.head())
+print("----------------")
+print(df['text'][:5])
+print("----------------")
+print(tok.encode(df['input'][0]))
