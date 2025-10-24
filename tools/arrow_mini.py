48550a44d498ca65232a1cdae3ff46d368da55f6
JokerQianwei <1174665346@qq.com>
2025-10-24 18:22:59 +0800

    修复 arrow_mini 导入路径

--- tools/arrow_mini.py
+++ tools/arrow_mini.py
@@ -1,12 +1,14 @@
 import pyarrow as pa
 import pyarrow.ipc as ipc
-from tokenizer import RegexSmilesTokenizer
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from src.tokenizer import RegexSmilesTokenizer
 import numpy as np
 tok = RegexSmilesTokenizer("vocab.txt")
 
 # 使用绝对路径
-file_path = '/data/yqw/smiles-bd/cache/cache-DrugLikeSMILSE-debug/validation/data-00000-of-00001.arrow'
-
+file_path = '/data/yqw/smiles-bd/cache/cache-DrugLikeSMILSE-debug/train/data-00000-of-00001.arrow'
 with open(file_path, "rb") as f:
     try:
         reader = ipc.open_file(f)
@@ -18,6 +20,3 @@ with open(file_path, "rb") as f:
 
 print(df.head())
 print("----------------")
-print(df['text'][:5])
-print("----------------")
-print(tok.encode(df['input'][0]))
